92b4d22a7abfbe83002cf86b47fb41119031fa9e
Himo <himo@Himos-MacBook-Pro.local>
2023-04-09 00:02:05 -0700

    structing application

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
+import config from "config";
+import helmet from "helmet";
+import Debug from "debug";
 import express from "express";
 import bodyParser from "body-parser";
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-
-dotenv.config();
-
 import routers from "./startup/routes";
-import swaggerDocs from "./utils/swagger";
-
-const port = Number(process.env.PORT) || 6000;
+const debug = Debug("app:startup");
 const app = express();
+
+console.log(config.get("name"));
+const port = Number(process.env.PORT) || 6000;
+
+app.use(helmet());
+app.use(express.static("public"));
 
 app.use(bodyParser.json());
 
 app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
-  routers(app, port);
-
-  // swaggerDocs(app, port);
+    debug("Application started");
+    console.log(`Server started at http://localhost:${port}`);
+    routers(app, port);
 });
 
 export default app;
--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -3,12 +3,10 @@ import healthCheck from "../routes/healthCheck";
 import cors from "cors";
 import morgan from "morgan";
 import swaggerDocs from "../utils/swagger";
-import validateResource from "../middleware/validateResource";
 import users from "../routes/users";
 
 export default function (app: Express, port: number) {
     app.use(express.json());
-
     app.use(morgan("tiny"));
     app.use(cors());
     app.use("/api/healthcheck", healthCheck);
